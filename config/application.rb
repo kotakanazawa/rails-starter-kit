a41c73bb9f67c1033fbd3cf955afe76cbbb82189
Kota Kanazawa <kota0818@gmail.com>
2020-09-27 11:08:37 +0900

    Set locale to JPN

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,9 @@ module RailsStarter
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = "Asia/Tokyo"
+    config.i18n.default_locale = :ja
+    
     config.generators do |g|
       g.test_framework :rspec,
             view_specs: false,
